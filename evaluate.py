beaf001227d968ccc1de7a494a1f52bf95d5eebc
chan991112 <115198519+chan991112@users.noreply.github.com>
2024-02-12 15:45:50 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -35,6 +35,7 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
 
     # Evaluation
     loss_sums = [0 for _ in range(6)]
+    #loss_sums array 초기화, fastspeech2loss의 return이 6개라서
     for batchs in loader:
         for batch in batchs:
             batch = to_device(batch, device)
@@ -47,9 +48,9 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
 
                 for i in range(len(losses)):
                     loss_sums[i] += losses[i].item() * len(batch[0])
-
+                    #losses의 개수만큼 반복, losses[i]에 batch_size를 곱해줌, 각 loss들의 sum을 loss sums에 저장. total_loss,  mel_loss, postnet_mel_loss, pitch_loss, energy_loss, duration_loss,
     loss_means = [loss_sum / len(dataset) for loss_sum in loss_sums]
-
+    #각 loss마다 mean을 계산
     message = "Validation Step {}, Total Loss: {:.4f}, Mel Loss: {:.4f}, Mel PostNet Loss: {:.4f}, Pitch Loss: {:.4f}, Energy Loss: {:.4f}, Duration Loss: {:.4f}".format(
         *([step] + [l for l in loss_means])
     )
@@ -62,6 +63,7 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
             model_config,
             preprocess_config,
         )
+        #utils-tools 파일 보면 fig는 pred, ground_truth 의 두가지 mel을 담는다. wav_reconstruction은 target을 vocoder에 넣어서 만든 wave form, wav_prediction은 prediction을 wavform으로 만든 것
 
         log(logger, step, losses=loss_means)
         log(
@@ -69,6 +71,7 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
             fig=fig,
             tag="Validation/step_{}_{}".format(step, tag),
         )
+        #validation log에 fig 기록
         sampling_rate = preprocess_config["preprocessing"]["audio"]["sampling_rate"]
         log(
             logger,
@@ -76,12 +79,14 @@ def evaluate(model, step, configs, logger=None, vocoder=None):
             sampling_rate=sampling_rate,
             tag="Validation/step_{}_{}_reconstructed".format(step, tag),
         )
+        #validation log에 wav_reconstruction기록
         log(
             logger,
             audio=wav_prediction,
             sampling_rate=sampling_rate,
             tag="Validation/step_{}_{}_synthesized".format(step, tag),
         )
+        #validation log에 wav_prediction 기록
 
     return message
 
@@ -97,13 +102,17 @@ if __name__ == "__main__":
         required=True,
         help="path to preprocess.yaml",
     )
+    #-p의 default 값은 "--preprocess_config"
     parser.add_argument(
         "-m", "--model_config", type=str, required=True, help="path to model.yaml"
     )
     parser.add_argument(
         "-t", "--train_config", type=str, required=True, help="path to train.yaml"
     )
+    #--restore_step부터 --train_config까지 인자값을 등록
     args = parser.parse_args()
+    #입력받은 인자값을 args에 저장
+
 
     # Read Config
     preprocess_config = yaml.load(
@@ -112,6 +121,7 @@ if __name__ == "__main__":
     model_config = yaml.load(open(args.model_config, "r"), Loader=yaml.FullLoader)
     train_config = yaml.load(open(args.train_config, "r"), Loader=yaml.FullLoader)
     configs = (preprocess_config, model_config, train_config)
+    #yaml file에서 불러와서 각각 preprocess_config, model_config, train_config, configs 변수에 저장
 
     # Get model
     model = get_model(args, configs, device, train=False).to(device)
