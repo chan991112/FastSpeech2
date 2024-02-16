a75e8dc4790ec0d6a3cb7246347bc1d77fe12012
chan991112 <115198519+chan991112@users.noreply.github.com>
2024-02-17 02:16:04 +0900

    Update Models.py

--- transformer/Models.py
+++ transformer/Models.py
@@ -41,7 +41,7 @@ class Encoder(nn.Module):
         n_src_vocab = len(symbols) + 1
         #source에 있는 어휘 총 개수
         d_word_vec = config["transformer"]["encoder_hidden"]
-        #encoder의 word embedding 차원..?d_model이랑 똑같은거 아닌가: 256
+        #encoder의 word embedding 차원: 256
         n_layers = config["transformer"]["encoder_layer"]
         #encoder layer의 수: 4
         n_head = config["transformer"]["encoder_head"]
@@ -50,13 +50,13 @@ class Encoder(nn.Module):
             config["transformer"]["encoder_hidden"]
             // config["transformer"]["encoder_head"]
         )
-        #attention의 ket와 value 차원: 256/2
+        #attention의 key와 value 차원: 256/2
         d_model = config["transformer"]["encoder_hidden"]
         #임베딩 차원: 256
         d_inner = config["transformer"]["conv_filter_size"]
         #feedforward network inner layer의 차원: 1024
         kernel_size = config["transformer"]["conv_kernel_size"]
-        #feedforward network convolution kernel의 크기...?
+        #feedforward network convolution kernel의 크기
         dropout = config["transformer"]["encoder_dropout"]
         #drop out 비율: [9:1]
         self.max_seq_len = config["max_seq_len"]
@@ -101,7 +101,7 @@ class Encoder(nn.Module):
             )[: src_seq.shape[1], :].unsqueeze(0).expand(batch_size, -1, -1).to(
                 src_seq.device
             )
-            #모델이 학습 중이 아니고, 입력 seq의 길이가 최대보다 크면..?클 수가 있나?
+            #모델이 학습 중이 아니고, 입력 seq의 길이가 최대보다 크면
         else:
             enc_output = self.src_word_emb(src_seq) + self.position_enc[
                 :, :max_len, :
@@ -126,17 +126,17 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
 
         n_position = config["max_seq_len"] + 1
-        d_word_vec = config["transformer"]["decoder_hidden"]
-        n_layers = config["transformer"]["decoder_layer"]
-        n_head = config["transformer"]["decoder_head"]
+        d_word_vec = config["transformer"]["decoder_hidden"]#256
+        n_layers = config["transformer"]["decoder_layer"]   #6
+        n_head = config["transformer"]["decoder_head"]      #2
         d_k = d_v = (
-            config["transformer"]["decoder_hidden"]
+            config["transformer"]["decoder_hidden"]         #256//2 key와 value의 dim=hidden//head
             // config["transformer"]["decoder_head"]
         )
-        d_model = config["transformer"]["decoder_hidden"]
-        d_inner = config["transformer"]["conv_filter_size"]
-        kernel_size = config["transformer"]["conv_kernel_size"]
-        dropout = config["transformer"]["decoder_dropout"]
+        d_model = config["transformer"]["decoder_hidden"]  #256
+        d_inner = config["transformer"]["conv_filter_size"]#1024
+        kernel_size = config["transformer"]["conv_kernel_size"] #[9,1] convolution 연산 할 때 9개에 대해서 1개의 값으로 뱉음
+        dropout = config["transformer"]["decoder_dropout"] #0.2 dropout은 overfitting을 막기 위해 20%의 뉴런을 다음 layer로 넘기지 않는다
 
         self.max_seq_len = config["max_seq_len"]
         self.d_model = d_model
@@ -152,6 +152,7 @@ class Decoder(nn.Module):
                     d_model, n_head, d_k, d_v, d_inner, kernel_size, dropout=dropout
                 )
                 for _ in range(n_layers)
+                #FFTBlock layer 수 만큼 쌓아줌
             ]
         )
         #여기까지 구조는 encoder와 똑같음
@@ -168,7 +169,7 @@ class Decoder(nn.Module):
             # -- Prepare masks
             slf_attn_mask = mask.unsqueeze(1).expand(-1, max_len, -1)
             #학습 중이 아니고 입력 시퀀스의 길이가 최대 시퀀스의 길이보다 클 경우
-            #encoder랑 다르게 이런 마스크를 미리 하나 만듦, 근데 안 쓰는데 왜 해줬지..?
+            #encoder랑 다르게 이런 마스크를 미리 하나 만듦
             dec_output = enc_seq + get_sinusoid_encoding_table(
                 enc_seq.shape[1], self.d_model
             )[: enc_seq.shape[1], :].unsqueeze(0).expand(batch_size, -1, -1).to(
@@ -177,9 +178,10 @@ class Decoder(nn.Module):
             #인코딩된 시퀀스에 위치 인코딩 더해줌
         else:
             max_len = min(max_len, self.max_seq_len)
-            #max_len으로 현재 길이와 최대 길이 중 작은 값으로 선택..?
+            #max_len으로 현재 길이와 최대 길이 중 작은 값으로 선택
 
             # -- Prepare masks
+            #attention 할 때 이후의 값을 0으로 설정해주는 마스크
             slf_attn_mask = mask.unsqueeze(1).expand(-1, max_len, -1)
             #else 문에서도 mask 선언
             dec_output = enc_seq[:, :max_len, :] + self.position_enc[
@@ -187,6 +189,7 @@ class Decoder(nn.Module):
             ].expand(batch_size, -1, -1)
             # enc_seq[:, :max_len, :]의 의미: 우선 얘는 3D tensor임.
             #첫 번째 축(batch)은 모두 선택, 두번째 축 (시퀀스 길이)에서는 처음부터 max_len 까지 선택하고, 세번째 축에서는 모두 선택한다.
+            #길이 맞춰주려고 넣은 padding 값을 무시하려고 사용하는 mask
             mask = mask[:, :max_len]
             slf_attn_mask = slf_attn_mask[:, :, :max_len]
         #여기까지 decoder에 encoder에서 온 정보 positional encoding 해주고 넣어주는 과정
@@ -200,4 +203,4 @@ class Decoder(nn.Module):
                 dec_slf_attn_list += [dec_slf_attn]
             #각 layer마다 attention 결과를 갱신
         return dec_output, mask
-        #mask는 왜 return하는 거지..?
+       
